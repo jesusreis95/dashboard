a1c2ee30bc70f68e0b10122afdc2061be9e37ced
jesusreis <jesus.reis@acirpriopreto.com.br>
2024-04-30 09:34:38 -0300

    update configs

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ const app = express();
 app.use(express.static('./dist/dashboard'));
 app.get('/*', (req, res) => res.sendFile('index.html', {root: 'dist/dashboard/'}));
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
